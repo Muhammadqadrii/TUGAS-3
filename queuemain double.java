93e8b5f3b98b7ff1028a42f872ae4704d9247692
muhammadqadrii <43694412+Muhammadqadrii@users.noreply.github.com>
2018-10-31 23:13:23 +0700

    Create queuemain double.java

---
+++ queuemain double.java
@@ -0,0 +1,18 @@
+public class classmainqueuedouble {
+
+	public static void main(String[] args) {
+
+		classqueuedouble q=new classqueuedouble();
+		
+		q.cetak();
+		q.insert(1.0);
+		q.cetak();
+		q.insert(2.0);
+		q.cetak();
+		q.insert(3.0);
+		q.cetak();
+
+
+	}
+
+}
