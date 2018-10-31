2effd7cfef9cd0301daf9e638b8d72c59c2d5d98
muhammadqadrii <43694412+Muhammadqadrii@users.noreply.github.com>
2018-10-31 23:11:33 +0700

    Create queuemain float.java

---
+++ queuemain float.java
@@ -0,0 +1,15 @@
+public class classmainqueuefloat {
+
+	public static void main(String[] args) {
+	
+		classqueuefloat q=new classqueuefloat();
+		q.cetak();
+		q.insert(1.0);
+		q.cetak();
+		q.insert(2.0);
+		q.cetak();
+		q.insert(3.0);
+		q.cetak();
+	}
+
+}
