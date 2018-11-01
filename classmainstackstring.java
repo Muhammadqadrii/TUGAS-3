cbdf7126d4dd4afd2df1d77b9a5adf7f105ff698
muhammadqadrii <43694412+Muhammadqadrii@users.noreply.github.com>
2018-11-01 16:40:38 +0800

    Update and rename classstackstring.java to classmainstackstring.java

---
+++ classmainstackstring.java
@@ -0,0 +1,27 @@
+package qadriii;
+
+
+public class classmainstackstring {
+
+	
+	public static void main(String[] args) {
+		classstackstring st= new classstackstring();
+		
+		
+		st.push("qadrii");
+		st.push("fandhy");
+		st.push("ucup");
+		st.push("masdar");
+		st.push("mufli");
+		
+		
+		st.push("anmar");
+		
+		String d0=st.pop();
+		System.out.println(d0);
+		st.cetak();
+
+
+	}
+
+}
